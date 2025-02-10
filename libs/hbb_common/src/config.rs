c88fa4d1ef925d4dfb8adf8d80c4999601a09d64
fslandy <100591126+fslandy@users.noreply.github.com>
2025-02-10 21:39:30 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -105,7 +105,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.8889642.xyz"];
-pub const PUBLIC_RS_PUB_KEY: &str = "FHZtUydBEC332ci0CWw1IHvCdosXcWS33EKsKyPMnEE=";
+pub const PUBLIC_RS_PUB_KEY: &str = "auelQyy4haEhPVCZXovFmhat1C4lpD38iPxLG07ryF4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
